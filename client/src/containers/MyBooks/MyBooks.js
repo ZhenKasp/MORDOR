88fd02227dff1af7a2010addb3da6c2c66befecd
Yauheni <zhenkasp@mail.ru>
2020-10-31 14:30:34 +0300

    show books on MyBooks

--- client/src/containers/MyBooks/MyBooks.js
+++ client/src/containers/MyBooks/MyBooks.js
@@ -1,33 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
+import CardDeck from 'react-bootstrap/CardDeck';
 import CreateBookModal from '../CreateBookModal/CreateBookModal';
+import BookCard from '../../components/BookCard/BookCard';
+import axios from 'axios';
+import classes from './MyBooks.module.css'
 
 const MyBooks = (props) => {
-  const [modalIsShown, modalIsShownSet] = useState(false);
+  const [modalIsShown, setModalIsShown] = useState(false);
+  const [books, setBooks] = useState([]);
 
-  const modalIsShownHandler = () => {
-    modalIsShownSet(true);
-  }
+  const modalIsShownHandler = () => setModalIsShown(true);
+  const modalIsShownCancelHandler = () => setModalIsShown(false);
 
-  const modalIsShownCancelHandler = () => {
-    modalIsShownSet(false);
-  }
+  useEffect(() => {
+    try {
+      axios.get(process.env.REACT_APP_PATH_TO_SERVER + "books", { headers: { authorization: localStorage.getItem('token') }})
+      .then(res => {
+        if (res.data.error) {
+          props.createFlashMessage(res.data.error, res.data.variant);
+        } else {
+          setBooks(res.data.books);
+        }
+      });
+    } catch (err) {
+      props.createFlashMessage(err.message, "danger");
+    }
+  }, [])
 
   return (
     <div>
-      <div>
-        <h3>My Books</h3>
-      </div>
-      <div>
-        <Button onClick={() => modalIsShownHandler()}>
-          Create New Book
-        </Button>
-      </div>
+      <h2>My Books</h2>
+      <Button onClick={() => modalIsShownHandler()}>
+        Create New Book
+      </Button>
       <CreateBookModal
         createFlashMessage={props.createFlashMessage}
         modalIsShownHandler={modalIsShownHandler}
         modalIsShownCancelHandler={modalIsShownCancelHandler}
         modalIsShown={modalIsShown} />
+
+      <CardDeck className={classes.BooksGrid}>
+      {books.map(book => (
+        <BookCard book={book} key={book.id}>
+          <Button variant="primary">Edit</Button>
+          <Button variant="success">Read</Button>
+          <Button variant="danger">Delete</Button>
+        </BookCard>
+      ))}
+    </CardDeck>
     </div>
   )
 }
--- client/src/containers/TagsInput/TagsInput.js
+++ client/src/containers/TagsInput/TagsInput.js
@@ -8,7 +8,8 @@ const TagsInput = (props) => {
 
   useEffect(() => {
     try {
-      axios.get(process.env.REACT_APP_PATH_TO_SERVER + "tags", { headers: { authorization: localStorage.getItem('token') }})
+      axios.get(process.env.REACT_APP_PATH_TO_SERVER + "tags",
+        { headers: { authorization: localStorage.getItem('token') }})
       .then(res => {
         if (res.data.error) {
           props.createFlashMessage(res.data.error, res.data.variant);
